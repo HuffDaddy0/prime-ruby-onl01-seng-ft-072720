65e954e5c45a4881fae8cf26c2f6bf51142182cd
Tyler Huffman <ty.huff24@gmail.com>
2020-08-04 21:37:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -43,7 +43,7 @@ return false if !num.integer?
 return false if num < 2
 return true if num == 2
   if num.odd?
-    (3..x - 1).to_a.none? do |tes|
+    (3..num - 1).to_a.none? do |tes|
       num % tes == 0
   end
 end
